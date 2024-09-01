8153fa55575521e986aff89f97a195e887f905a1
Shiven Patel <shiven110504@gmail.com>
2024-09-01 00:18:09 -0400

    Changed FAQ Content

--- components/Landing/Faq.js
+++ components/Landing/Faq.js
@@ -15,10 +15,10 @@ export default function Faq() {
             hackathon?
           </h3>
           <p className="FAQContentItemText">
-            A hackathon is where students bring their cool ideas to life. Over
-            the course of 36 hours, participants can use software and/or
-            hardware to build their idea and win prizes, whether as a team or as
-            an individual.
+            Hackathons are events where programmers come together to collaborate
+            on innovative software and hardware projects. You’re encouraged to
+            build anything your imagination inspires! “Innovation is the ability
+            to see change as an opportunity–not a threat.” - Steve Jobs
           </p>
         </div>
         <div className="FAQContentItem">
@@ -27,32 +27,12 @@ export default function Faq() {
             <FontAwesomeIcon icon={faCaretRight} /> &nbsp;&nbsp;Where is it?
           </h3>
           <p className="FAQContentItemText">
-            HackUMass XII will be completely in-person this year. This includes
-            check-in, hardware checkout, swag pickup, participation and judging.
-            Mentoring and most workshops will also take place in-person. More
-            details regarding the career fair, opening and closing ceremonies,
-            and catering will be given closer to the hackathon.
-          </p>
-        </div>
-        <div className="FAQContentItem">
-          <h3 className="FAQContentItemHeading">
-            {" "}
-            <FontAwesomeIcon icon={faCaretRight} /> &nbsp;&nbsp;Who's coming?
-          </h3>
-          <p className="FAQContentItemText">
-            We expect over 1000 hackers from across the U.S. to attend our
-            hackathon.
-          </p>
-        </div>
-        <div className="FAQContentItem">
-          <h3 className="FAQContentItemHeading">
-            {" "}
-            <FontAwesomeIcon icon={faCaretRight} /> &nbsp;&nbsp;Who can attend?
-          </h3>
-          <p className="FAQContentItemText">
-            Participation in HackUMass XII is not limited to UMass Amherst
-            students. The hackathon and the workshops are open to all
-            college/university students, across all majors!
+            HackUMass XII is an in-person hackathon hosted by UMass Amherst.
+            This includes check-in, hardware checkout, swag pickup,
+            participation and judging. Mentoring and most workshops will also
+            take place in-person. More details regarding the career fair,
+            opening and closing ceremonies, and catering will be given closer to
+            the hackathon!
           </p>
         </div>
         <div className="FAQContentItem">
@@ -62,24 +42,24 @@ export default function Faq() {
             experience?
           </h3>
           <p className="FAQContentItemText">
-            We welcome hackers of all experience levels to HackUMass! Our
-            beginner track will help hackers who want to learn more about
-            software and hardware hacking get started. We will also have mentors
-            willing to help if you run into any problems during the event.
+            We’re excited to welcome hackers of all experience levels to
+            HackUMass! Whether you’re new to software and hardware hacking or
+            looking to sharpen your skills, our beginner track is designed to
+            help you get started with confidence. Plus, our team of mentors will
+            be on hand throughout the event to guide you through any challenges
+            you encounter.
           </p>
         </div>
         <div className="FAQContentItem">
           <h3 className="FAQContentItemHeading">
             {" "}
-            <FontAwesomeIcon icon={faCaretRight} /> &nbsp;&nbsp;Should I join a
-            team?
+            <FontAwesomeIcon icon={faCaretRight} /> &nbsp;&nbsp;Who can attend?
           </h3>
           <p className="FAQContentItemText">
-            We encourage everyone to work in a team of up to 4 people. Larger
-            teams are allowed, but we are unable to guarantee that every member
-            of your team will be able to receive prizes if you win. There will
-            be plenty of opportunities to form your team online and at the event
-            -- so don't worry if you haven't yet planned it out.
+            HackUMass XII is open to all college and university students,
+            regardless of major! Our goal is for you to leave with new
+            experiences, lasting friendships, and, of course, a satisfied
+            appetite.
           </p>
         </div>
         <div className="FAQContentItem">
@@ -89,8 +69,38 @@ export default function Faq() {
             project before the event?
           </h3>
           <p className="FAQContentItemText">
-            No, you may not start your project beforehand. All of the work
-            related to the project must be done during the event.
+            While getting a head start is a smart strategy, we require every
+            participant to start their project during the hours of hacking. Any
+            hint of a project being created/edited before the event will cause
+            instant disqualification.
+          </p>
+        </div>
+        <div className="FAQContentItem">
+          <h3 className="FAQContentItemHeading">
+            {" "}
+            <FontAwesomeIcon icon={faCaretRight} /> &nbsp;&nbsp;Should I be in a
+            team?
+          </h3>
+          <p className="FAQContentItemText">
+            Absolutely! We highly encourage everyone to get in a team of 4 or
+            less. There is no stopping you from going at it solo or working
+            within a team of more than 4. However, we are unable to guarantee
+            that every member of your team will be able to receive prizes if you
+            win. There will be plenty of opportunities to form your team online
+            and at the event -- so don't worry if you haven't yet planned it
+            out.
+          </p>
+        </div>
+        <div className="FAQContentItem">
+          <h3 className="FAQContentItemHeading">
+            {" "}
+            <FontAwesomeIcon icon={faCaretRight} /> &nbsp;&nbsp;Will my travel
+            be reimbursed?
+          </h3>
+          <p className="FAQContentItemText">
+            Unfortunately we are unable to reimburse travel expenses for
+            HackUMass XII. Kindly reach out to us if you have any concerns or
+            questions regarding this.
           </p>
         </div>
         <div className="FAQContentItem">
@@ -100,8 +110,9 @@ export default function Faq() {
             of Conduct we need to abide by?
           </h3>
           <p className="FAQContentItemText">
-            Be smart about your actions. We enforce the MLH Code of Conduct at
-            our event.
+            We want to create a safe environment for participants, speakers, and
+            sponsors. For this reason, we expect everyone to follow the MLH Code
+            of Conduct.
           </p>
         </div>
       </div>
