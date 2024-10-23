4d64cc4955c88af4f5151b3f1453ff3a9d8f6cee
gamer-1478 <aayushgarg2006@gmail.com>
2024-10-23 17:31:54 -0400

    Added new sponsors and partners to the landing page
    - Moved old sponsors to the Archive folder
    - Added new sponsors to the Sponsors folder
    - Added new partners to the Partners folder
    - Deleted old sponsors
    - Updated the Sponsors.js file to reflect the changes
    - Updated the Landing.css file to reflect the changes

--- components/Landing/Sponsors.js
+++ components/Landing/Sponsors.js
@@ -9,50 +9,184 @@ export default function Sponsors() {
       <div className="SponsorImages">
         <div className="SponsorImagesUpper">
           <Image
-            src="/SponsorCOE.png"
+            src="/Sponsors/SponsorCOE.png"
             sizes={2000}
             width={0}
             height={0}
             priority={true}
+            onClick={() => window.open("https://engineering.umass.edu/", "_blank")}
           />
           <Image
-            src="/SponsorCICS.png"
+            src="/Sponsors/SponsorCICS.png"
             sizes={2000}
             width={0}
             height={0}
             priority={true}
+            onClick={() => window.open("https://www.umass.edu/cics/", "_blank")}
+
           />
           <Image
-            src="/SponsorBE.png"
+            src="/Sponsors/SponsorBE.png"
             sizes={2000}
             width={0}
             height={0}
             priority={true}
+            onClick={() => window.open("https://www.umass.edu/entrepreneurship/", "_blank")}
           />
         </div>
         <div className="SponsorImagesUpper">
           <Image
-            src="/SponsorKPMG.png"
+            src="/Sponsors/STR_Sponsor.png"
             sizes={2000}
             width={0}
             height={0}
             priority={true}
-          />
-          <Image
-            src="/SponsorMitre.png"
-            sizes={2000}
-            width={0}
-            height={0}
-            priority={true}
-          />
-          <Image
-            src="/SponsorStandOutSticker.png"
-            sizes={2000}
-            width={0}
-            height={0}
-            priority={true}
+            onClick={() => window.open("https://www.str.com/", "_blank")}
           />
         </div>
+      </div>
+      <div className="LandingAboutHeader">
+        <h1 className="heading">Our Partners</h1>
+      </div>
+      <div className="PartnerImages">
+        <div className="PartnerImagesUpper">
+          <Image
+            src="/Partners/saily-color.png"
+            sizes={2000}
+            width={0}
+            height={0}
+            priority={true}
+            onClick={() => window.open("https://saily.com/", "_blank")}
+          />
+          <Image
+            src="/Partners/wolfram-corporate-logo-horizontal-1-removebg-preview.png"
+            sizes={2000}
+            width={0}
+            height={0}
+            priority={true}
+            onClick={() => window.open("https://www.wolframalpha.com/", "_blank")}
+          />
+          <Image
+            src="/Partners/insomnia-cookies.png"
+            sizes={2000}
+            width={0}
+            height={0}
+            priority={true}
+            onClick={() => window.open("https://insomniacookies.com/", "_blank")}
+          />
+          <Image
+            src="/Partners/nordpass-color-horizontal.png"
+            sizes={2000}
+            width={0}
+            height={0}
+            priority={true}
+            onClick={() => window.open("https://nordpass.com/", "_blank")}
+          />
+          <Image
+            src="/Partners/M5Logo-removebg-preview.png"
+            sizes={2000}
+            width={0}
+            height={0}
+            priority={true}
+            onClick={() => window.open("https://www.umassamherstm5.org/", "_blank")}
+          />
+
+        </div>
+        <div className="PartnerImagesUpper">
+          <Image
+            src="/Partners/CICS-makerspace.png"
+            sizes={2000}
+            width={0}
+            height={0}
+            priority={true}
+            onClick={() => window.open("https://groups.cs.umass.edu/makerspace/", "_blank")}
+          />
+          <Image
+            src="/Partners/silverscreen.png"
+            sizes={2000}
+            width={0}
+            height={0}
+            priority={true}
+            onClick={() => window.open("https://www.silverscreendesign.com/", "_blank")}
+          />
+          <Image
+            src="/Partners/StandOut-Stickers-Logo.png"
+            sizes={2000}
+            width={0}
+            height={0}
+            priority={true}
+            onClick={() => window.open("https://www.standoutstickers.com/", "_blank")}
+          />
+          <Image
+            src="/Partners/MakerspaceVertical.png"
+            sizes={2000}
+            width={0}
+            height={0}
+            priority={true}
+            onClick={() => window.open("https://sites.google.com/umass.edu/umassmakerspace/", "_blank")}
+          />
+          <Image
+            src="/Partners/incogni-black.png"
+            sizes={2000}
+            width={0}
+            height={0}
+            priority={true}
+            onClick={() => window.open("https://incogni.com/", "_blank")}
+          />
+
+          <Image
+            src="/Partners/Makerspace-Logo2.png"
+            sizes={2000}
+            width={0}
+            height={0}
+            priority={true}
+            onClick={() => window.open("https://sites.google.com/umass.edu/umassmakerspace/", "_blank")}
+          />
+
+        </div>
+        <div className="PartnerImagesUpper">
+          <Image
+            src="/Partners/balsamiq-logo-screen.png"
+            sizes={2000}
+            width={0}
+            height={0}
+            priority={true}
+            onClick={() => window.open("https://balsamiq.com/", "_blank")}
+          />
+          <Image
+            src="/Partners/mlh-logo-color.png"
+            sizes={2000}
+            width={0}
+            height={0}
+            priority={true}
+            onClick={() => window.open("https://mlh.io/", "_blank")}
+          />
+          <Image
+            src="/Partners/nordVPN-color.png"
+            sizes={2000}
+            width={0}
+            height={0}
+            priority={true}
+            onClick={() => window.open("https://nordvpn.com/", "_blank")}
+          />
+          <Image
+            src="/Partners/nordpass-color-vertical.png"
+            sizes={2000}
+            width={0}
+            height={0}
+            priority={true}
+            onClick={() => window.open("https://nordpass.com/", "_blank")}
+          />
+          <Image
+            src="/Partners/ADI-Logo-AWP-Tagline-RGB-FullColor.jpg"
+            sizes={2000}
+            width={0}
+            height={0}
+            priority={true}
+            onClick={() => window.open("https://www.analog.com/", "_blank")}
+          />
+        </div>
+
       </div>
       <div className="WhySponsor">
         <div className="WhySponsorLeft">
@@ -70,7 +204,7 @@ export default function Sponsors() {
         </div>
         <div className="WhySponsorRight">
           <Image
-            src="/SponsorUsRight.png"
+            src="/Sponsors/SponsorUsRight.png"
             sizes={2000}
             width={0}
             height={0}
