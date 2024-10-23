7d9c8b006cb2f0556aeed7ba1804408c82f90b03
arhammusheer <arhammusheer@gmail.com>
2024-10-23 01:00:31 -0400

    Removed dangerously set native javascript to use native react functions

--- components/Landing/OurTeam.tsx
+++ components/Landing/OurTeam.tsx
@@ -1,170 +1,83 @@
-import { useEffect } from "react";
+import { useState } from "react";
 import { motion } from "framer-motion";
 import "./OurTeam.css";
 import "./Landing.css";
 
-export default function OurTeam() {
-  useEffect(() => {
-    const members = {
-      Directors: [
-        ["Dev Mehta", "Co-Director"],
-        ["Khiem Nguyen", "Co-Director"],
-      ],
-      Hardware: [
-        ["Kavya Manchanda", "Hardware Co-Director"],
-        ["Poul Holm", "Hardware Co-Director"],
-        ["Aayan Boradia", "Hardware Member"],
-        ["Ananya Rohatgi", "Hardware Member"],
-        ["Devaj Chopda", "Hardware Member"],
-        ["Franka Yakasovic", "Hardware Member"],
-        ["Jessie Wang", "Hardware Member"],
-        ["Monil Bhavsar", "Hardware Member"],
-        ["Ron Bajrami", "Hardware Member"],
-        ["Sreeniyathi Kasireddy", "Hardware Member"],
-      ],
-      Tech: [
-        ["Shiven Patel", "Technology Director"],
-        ["Aayush Garg", "Technology Member"],
-        ["Manu Hegde", "Technology Member"],
-        ["Manya Phutela", "Technology Member"],
-        ["Sathvik Yadanaparthi", "Technology Member"],
-        ["Shreyas Donti", "Technology Member"],
-      ],
-      "PR-Design": [
-        ["Vianca Varma", "PR/Design Director"],
-        ["Anay Liu", "PR/Design Member"],
-        ["Hanabel Mengistu", "PR/Design Member"],
-        ["Naina Singh", "PR/Design Member"],
-        ["Swastika Dev", "PR/Design Member"],
-      ],
-      Sponsorship: [
-        ["Renee Tsang", "Sponsorship Director"],
-        ["Joud Zamzami", "Sponsorship Member"],
-        ["Mikael Sareshwala", "Sponsorship Member"],
-        ["Tawsif Ibne Azad", "Sponsorship Member"],
-      ],
-      Logistics: [
-        ["Addie Padhi", "Logistics Director"],
-        ["Aanya Mehta", "Logistics Member"],
-        ["Aarav Nair", "Logistics Member"],
-        ["Aarav Sehgal", "Logistics Member"],
-        ["Brian Zhang", "Logistics Member"],
-        ["Dhriti Madireddy", "Logistics Member"],
-        ["Uma Purani", "Logistics Member"],
-      ],
-    };
+interface IMembers {
+  [key: string]: string[][];
+}
 
-    /* Get tab objects */
-    const directorsTab = document.getElementById("nav-directors");
-    const hardwareTab = document.getElementById("nav-hardware");
-    const logisticsTab = document.getElementById("nav-logistics");
-    const prdesignTab = document.getElementById("nav-prdesign");
-    const sponsorshipTab = document.getElementById("nav-sponsorship");
-    const technologyTab = document.getElementById("nav-technology");
+const members: IMembers = {
+  Directors: [
+    ["Dev Mehta", "Co-Director"],
+    ["Khiem Nguyen", "Co-Director"],
+  ],
+  Hardware: [
+    ["Kavya Manchanda", "Hardware Co-Director"],
+    ["Poul Holm", "Hardware Co-Director"],
+    ["Aayan Boradia", "Hardware Member"],
+    ["Ananya Rohatgi", "Hardware Member"],
+    ["Devaj Chopda", "Hardware Member"],
+    ["Franka Yakasovic", "Hardware Member"],
+    ["Jessie Wang", "Hardware Member"],
+    ["Monil Bhavsar", "Hardware Member"],
+    ["Ron Bajrami", "Hardware Member"],
+    ["Sreeniyathi Kasireddy", "Hardware Member"],
+  ],
+  Tech: [
+    ["Shiven Patel", "Technology Director"],
+    ["Aayush Garg", "Technology Member"],
+    ["Manu Hegde", "Technology Member"],
+    ["Manya Phutela", "Technology Member"],
+    ["Sathvik Yadanaparthi", "Technology Member"],
+    ["Shreyas Donti", "Technology Member"],
+  ],
+  "PR-Design": [
+    ["Vianca Varma", "PR/Design Director"],
+    ["Anay Liu", "PR/Design Member"],
+    ["Hanabel Mengistu", "PR/Design Member"],
+    ["Naina Singh", "PR/Design Member"],
+    ["Swastika Dev", "PR/Design Member"],
+  ],
+  Sponsorship: [
+    ["Renee Tsang", "Sponsorship Director"],
+    ["Joud Zamzami", "Sponsorship Member"],
+    ["Mikael Sareshwala", "Sponsorship Member"],
+    ["Tawsif Ibne Azad", "Sponsorship Member"],
+  ],
+  Logistics: [
+    ["Addie Padhi", "Logistics Director"],
+    ["Aanya Mehta", "Logistics Member"],
+    ["Aarav Nair", "Logistics Member"],
+    ["Aarav Sehgal", "Logistics Member"],
+    ["Brian Zhang", "Logistics Member"],
+    ["Dhriti Madireddy", "Logistics Member"],
+    ["Uma Purani", "Logistics Member"],
+  ],
+};
 
-    const tabs = [
-      directorsTab,
-      hardwareTab,
-      logisticsTab,
-      prdesignTab,
-      sponsorshipTab,
-      technologyTab,
-    ];
+function OurTeam() {
+  const [activeTab, setActiveTab] = useState("Directors");
 
-    const createTeamArr = (team) => {
-      const people = members[team];
-      return people.map((e) => {
-        const fullName = e[0];
-        const role = e[1];
-        const [firstName, lastName] = fullName.split(" ");
-        return {
-          name: fullName,
-          role: role,
-          image: `/Team/${team}_${firstName}_${lastName}.jpg`,
-        };
-      });
-    };
+  const tabs = Object.keys(members);
 
-    const makeRowDivElement = () => {
-      const rowDivElement = document.createElement("div");
-      rowDivElement.classList.add(
-        "row",
-        "gap-y",
-        "justify-content-center",
-        "align-items-center",
-        "mt-3"
-      );
-      return rowDivElement;
-    };
-
-    const makeColDivElement = (memberObj) => {
-      const colDivElement = document.createElement("div");
-      colDivElement.classList.add(
-        "col-lg-3",
-        "col-md-6",
-        "col-sm-12",
-        "text-center",
-        "team-content"
-      );
-
-      const imageSrc = memberObj.image
-        ? memberObj.image
-        : "public/Team/Default.jpg";
-      
-      const memberContent = document.createElement("div");
-      memberContent.innerHTML = `
-        <motion.div 
-          initial={{ opacity: 0, y: 50 }} 
-          animate={{ opacity: 1, y: 0 }} 
-          transition={{ duration: 0.6 }}
-        >
-          <img class="rounded-circle shadow-2 img-border object-fit-cover" 
-              src="${imageSrc}" alt="${memberObj.name}" onerror="this.onerror=null;this.src='/Team/Default.jpg';" style=""/>
-          <h5 class="primary-color role-text">${memberObj.name}</h5>
-          <h6 class="team-role name-text">${memberObj.role}</h6>
-        </motion.div>
-      `;
-      colDivElement.appendChild(memberContent);
-
-      return colDivElement;
-    };
-
-    const teams = [
-      createTeamArr("Directors"),
-      createTeamArr("Hardware"),
-      createTeamArr("Logistics"),
-      createTeamArr("PR-Design"),
-      createTeamArr("Sponsorship"),
-      createTeamArr("Tech"),
-    ];
-
-    tabs.forEach((tab) => (tab.innerHTML = ""));
-
-    teams.forEach((team, i) => {
-      const row = makeRowDivElement();
-      team.forEach((memberObj) => {
-        row.appendChild(makeColDivElement(memberObj));
-      });
-      tabs[i].appendChild(row);
+  const createTeamArr = (team: string) => {
+    const people = members[team];
+    return people.map((e) => {
+      const fullName = e[0];
+      const role = e[1];
+      const [firstName, lastName] = fullName.split(" ");
+      return {
+        name: fullName,
+        role: role,
+        image: `/Team/${team}_${firstName}_${lastName}.jpg`,
+      };
     });
+  };
 
-    document.querySelectorAll(".nav-link").forEach((tab) => {
-      tab.addEventListener("click", function (e) {
-        e.preventDefault();
-
-        document
-          .querySelectorAll(".nav-link")
-          .forEach((link) => link.classList.remove("active"));
-        document
-          .querySelectorAll(".tab-pane")
-          .forEach((pane) => pane.classList.remove("show", "active"));
-
-        this.classList.add("active");
-        const target = document.querySelector(this.getAttribute("href"));
-        target.classList.add("show", "active");
-      });
-    });
-  }, []);
+  const handleTabClick = (team: string) => {
+    setActiveTab(team);
+  };
 
   return (
     <section>
@@ -183,114 +96,62 @@ export default function OurTeam() {
                       id="nav-tab"
                       role="tablist"
                     >
-                      <a
-                        className="nav-item nav-link active"
-                        id="nav-directors-tab"
-                        data-toggle="tab"
-                        href="#nav-directors"
-                        role="tab"
-                        aria-controls="nav-directors"
-                        aria-selected="true"
-                      >
-                        Directors
-                      </a>
-                      <a
-                        className="nav-item nav-link"
-                        id="nav-hardware-tab"
-                        data-toggle="tab"
-                        href="#nav-hardware"
-                        role="tab"
-                        aria-controls="nav-hardware"
-                        aria-selected="true"
-                      >
-                        Hardware
-                      </a>
-                      <a
-                        className="nav-item nav-link"
-                        id="nav-logistics-tab"
-                        data-toggle="tab"
-                        href="#nav-logistics"
-                        role="tab"
-                        aria-controls="nav-logistics"
-                        aria-selected="false"
-                      >
-                        Logistics
-                      </a>
-                      <a
-                        className="nav-item nav-link"
-                        id="nav-prdesign-tab"
-                        data-toggle="tab"
-                        href="#nav-prdesign"
-                        role="tab"
-                        aria-controls="nav-prdesign"
-                        aria-selected="false"
-                      >
-                        PR/Design
-                      </a>
-                      <a
-                        className="nav-item nav-link"
-                        id="nav-sponsorship-tab"
-                        data-toggle="tab"
-                        href="#nav-sponsorship"
-                        role="tab"
-                        aria-controls="nav-sponsorship"
-                        aria-selected="false"
-                      >
-                        Sponsorship
-                      </a>
-                      <a
-                        className="nav-item nav-link"
-                        id="nav-technology-tab"
-                        data-toggle="tab"
-                        href="#nav-technology"
-                        role="tab"
-                        aria-controls="nav-technology"
-                        aria-selected="false"
-                      >
-                        Technology
-                      </a>
+                      {Object.keys(members).map((team) => (
+                        <a
+                          key={team}
+                          className={`nav-item nav-link ${
+                            activeTab === team ? "active" : ""
+                          }`}
+                          onClick={() => handleTabClick(team)}
+                          href="#!"
+                        >
+                          {team}
+                        </a>
+                      ))}
                     </div>
                   </nav>
                   <div
                     className="tab-content py-3 px-3 px-sm-0"
                     id="nav-tabContent"
                   >
-                    <div
-                      className="tab-pane fade show active"
-                      id="nav-directors"
-                      role="tabpanel"
-                      aria-labelledby="nav-directors-tab"
-                    ></div>
-                    <div
-                      className="tab-pane fade"
-                      id="nav-hardware"
-                      role="tabpanel"
-                      aria-labelledby="nav-hardware-tab"
-                    ></div>
-                    <div
-                      className="tab-pane fade"
-                      id="nav-logistics"
-                      role="tabpanel"
-                      aria-labelledby="nav-logistics-tab"
-                    ></div>
-                    <div
-                      className="tab-pane fade"
-                      id="nav-prdesign"
-                      role="tabpanel"
-                      aria-labelledby="nav-prdesign-tab"
-                    ></div>
-                    <div
-                      className="tab-pane fade"
-                      id="nav-sponsorship"
-                      role="tabpanel"
-                      aria-labelledby="nav-sponsorship-tab"
-                    ></div>
-                    <div
-                      className="tab-pane fade"
-                      id="nav-technology"
-                      role="tabpanel"
-                      aria-labelledby="nav-technology-tab"
-                    ></div>
+                    {Object.keys(members).map((team) => (
+                      <div
+                        key={team}
+                        className={`tab-pane fade ${
+                          activeTab === team ? "show active" : ""
+                        }`}
+                      >
+                        <div className="row gap-y justify-content-center align-items-center mt-3">
+                          {createTeamArr(team).map((member, index) => (
+                            <div
+                              key={index}
+                              className="col-lg-3 col-md-6 col-sm-12 text-center team-content"
+                            >
+                              <motion.div
+                                initial={{ opacity: 0, y: 50 }}
+                                animate={{ opacity: 1, y: 0 }}
+                                transition={{ duration: 0.6 }}
+                              >
+                                <img
+                                  className="rounded-circle shadow-2 img-border object-fit-cover"
+                                  src={member.image}
+                                  alt={member.name}
+                                  onError={(e: any) =>
+                                    (e.target.src = "/Team/default.jpg")
+                                  }
+                                />
+                                <h5 className="primary-color role-text">
+                                  {member.name}
+                                </h5>
+                                <h6 className="team-role name-text">
+                                  {member.role}
+                                </h6>
+                              </motion.div>
+                            </div>
+                          ))}
+                        </div>
+                      </div>
+                    ))}
                   </div>
                 </div>
               </div>
@@ -301,3 +162,5 @@ export default function OurTeam() {
     </section>
   );
 }
+
+export default OurTeam;
