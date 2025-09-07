d788ee578622f20d92912df4bb42765ea21ec098
stelleron <shreyasdonti223@gmail.com>
2025-09-07 14:13:21 -0400

    Committing changes

--- components/Landing/Hero.js
+++ components/Landing/Hero.js
@@ -7,63 +7,65 @@ import { motion } from "framer-motion";
 
 export default function HeroSection() {
   return (
-    <div className="LandingContainer">
-      <div className="LandingHeroMain">
+    <div>
+      <div className="bg-black py-1 flex justify-center">
+        <h1 className="text-xl text-white text-center">
+          Check Back Soon for Updated Information on HackUMass XIII taking place Nov 7-9, 2025!
+        </h1>
+      </div>
+      <div className="LandingContainer">
+        <div className="LandingHeroMain">
+          <motion.div
+            initial={{ x: -300, opacity: 0 }} // Start position from left
+            animate={{ x: 0, opacity: 1 }} // End position
+            transition={{ duration: 1 }} // Animation duration
+          >
+            <Image
+              className="LandingHeroMainImg"
+              src="/HeroMain.png"
+              priority={true}
+              sizes={1000}
+              width={0}
+              height={0}
+            />
+          </motion.div>
+          <motion.button
+            className="LandingHeroMainAppbutt"
+            onClick={() =>
+              window.open("https://dashboard.hackumass.com/login", "_blank")
+            }
+            initial={{ x: -300, opacity: 0 }} // Start position from left
+            animate={{ x: 0, opacity: 1 }} // End position
+            transition={{ duration: 1 }} // Animation duration
+          >
+            <p style={{ margin: "auto" }}>Apply Now!</p>
+            <Fragment>
+              <FontAwesomeIcon
+                className="LandingHeroMainAppRightButton"
+                icon={faCircleArrowRight}
+                style={{ fontSize: "2.3vw", padding: 0 }}
+              />
+            </Fragment>
+          </motion.button>
+        </div>
+
         <motion.div
-          initial={{ x: -300, opacity: 0 }} // Start position from left
-          animate={{ x: 0, opacity: 1 }} // End position
-          transition={{ duration: 1 }} // Animation duration
+          className="LandingHeroBuilding"
+          initial={{ y: 400, opacity: 0 }} // Start position from bottom
+          animate={{ y: 0, opacity: 1 }} // End position
+          transition={{ duration: 1.5 }} // Animation duration
         >
+
           <Image
-            className="LandingHeroMainImg"
-            src="/HeroMain.png"
             priority={true}
-            sizes={1000}
+            className="LandingHeroBuildingImg"
+            src="/Building.svg"
+            sizes={5000}
             width={0}
             height={0}
           />
         </motion.div>
-        <motion.button
-          className="LandingHeroMainAppbutt"
-          onClick={() =>
-            window.open("https://dashboard.hackumass.com/login", "_blank")
-          }
-          initial={{ x: -300, opacity: 0 }} // Start position from left
-          animate={{ x: 0, opacity: 1 }} // End position
-          transition={{ duration: 1 }} // Animation duration
-        >
-          <p style={{ margin: "auto" }}>Apply Now!</p>
-          <Fragment>
-            <FontAwesomeIcon
-              className="LandingHeroMainAppRightButton"
-              icon={faCircleArrowRight}
-              style={{ fontSize: "2.3vw", padding: 0 }}
-            />
-          </Fragment>
-        </motion.button>
       </div>
-  
-      <motion.div
-        className="LandingHeroBuilding"
-        initial={{ y: 400, opacity: 0 }} // Start position from bottom
-        animate={{ y: 0, opacity: 1 }} // End position
-        transition={{ duration: 1.5 }} // Animation duration
-      >
-        
-        <Image
-          priority={true}
-          className="LandingHeroBuildingImg"
-          src="/Building.svg"
-          sizes={5000}
-          width={0}
-          height={0}
-        />
-      </motion.div>
-          <motion.div className="LandingHeroText" initial={{ y: -480, opacity: 0 }} // Start position from bottom
-        animate={{ y: -455, opacity: 1 }} // End position
-        transition={{ duration: 2.5 }} >
-            <p>Check Back Soon for Updated Information on HackUMass XIII taking place Nov 7-9, 2025!</p>
-          </motion.div>
     </div>
   );
 }
