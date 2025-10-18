e843b26007d88ffb4c968faccdcfff99565c9804
Sathvik Yadanaparthi <sathvik0423@gmail.com>
2025-10-18 18:41:47 -0400

    Add the MLH badge

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -164,6 +164,29 @@ function NavigationBarLand(user) {
           </ul>
         </div>
       </nav>
+
+      <a
+        id="mlh-trust-badge"
+        href="https://mlh.io/na?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2026-season&utm_content=white"
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{
+          display: "block",
+          maxWidth: "100px",
+          minWidth: "60px",
+          position: "fixed",
+          right: "50px",
+          top: "0",
+          width: "10%",
+          zIndex: "10000",
+        }}
+      >
+        <img
+          src="https://s3.amazonaws.com/logged-assets/trust-badge/2026/mlh-trust-badge-2026-white.svg"
+          alt="Major League Hacking 2026 Hackathon Season"
+          style={{ width: "100%" }}
+        />
+      </a>
     </>
   );
 }
